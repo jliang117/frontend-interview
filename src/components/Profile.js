61391b119e160527e3e0988da924a708cc10b551
jliang117 <jliang117@users.noreply.github.com>
2020-01-09 13:00:37 -0500

    fix updating user profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,32 +1,40 @@
 import * as React from "react";
-import { useUserContext } from "../contexts/user";
+import { useUserContext, updateUserContext } from "../contexts/user";
 
 /**
  * This should update the user context with the new values for email and name
  */
 const Profile = () => {
   const user = useUserContext();
-
+  const [updatedUserInfo, setUpdatedUserInfo] = React.useState({
+    name: user.name,
+    email: user.email
+  });
+  const handleUpdateUserInfo = (name, email) => {
+    setUpdatedUserInfo({name, email})
+  }
   return (
     <div>
       <h1>Edit your profile</h1>
       <form
         onSubmit={e => {
           e.preventDefault();
+          user.name = updatedUserInfo.name;
+          user.email = updatedUserInfo.email;
         }}
       >
         <input
           name="email"
-          value={user.email}
+          value={updatedUserInfo.email}
           onChange={e => {
-            user.email = e.target.value;
+            handleUpdateUserInfo(user.name, e.target.value)
           }}
         />
         <input
           name="name"
-          value={user.name}
+          value={updatedUserInfo.name}
           onChange={e => {
-            user.name = e.target.value;
+            handleUpdateUserInfo(e.target.value, user.email)
           }}
         />
         <button type="submit">Submit</button>
